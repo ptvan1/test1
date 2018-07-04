236d3dbaf6d8a2af6bb32aae83fdbecd1cd25b5f
TMA\ptvan1 <phungtrongvan3004@gmail.com>
2018-07-04 11:08:21 +0700

    4th edit

--- test3.java
+++ test3.java
@@ -1,1 +1,10 @@
-println("Hello World 3rd!!!")
+public class test3 {
+
+   /* This is my first java program.
+    * This will print 'Hello World' as the output
+    */
+
+   public static void main(String []args) {
+      System.out.println("Hello World"); // prints Hello World
+   }
+}
